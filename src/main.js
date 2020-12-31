03ecb9ab8c4738eb109e9dbfddf4849931b8edf6
独行 <jsw0325@163.com>
2020-12-31 19:30:07 +0800

    feat: 添加文章详情页布局

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './assets/style/reset.scss'
+import './assets/scss/style.scss'
+import './assets/scss/reset.scss'
 import axios from 'axios'
 
 const seivice = axios.create({
